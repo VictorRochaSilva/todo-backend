05ea5ba586b12fe4fa3b80d099258befffa279e3
Victor Rocha <victowrs.rocha@gmail.com>
2025-07-15 19:57:16 -0400

    atualizando para ficar na uri

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-const mongoUri = "mongodb://admin:senha123@mongodb:27017/?authSource=admin";
+const mongoUri = "mongodb://admin:senha123@mongodb:27017/admin?authSource=admin";
 
 async function connectMongo() {
     if (mongoose.connection.readyState >= 1) return;
 
     try {
-        await mongoose.connect(mongoUri, { dbName: "admin" });
+        await mongoose.connect(mongoUri);
         console.log("✅ Conectado ao MongoDB!");
     } catch (err) {
         console.error("❌ Erro ao conectar no MongoDB:", err);
