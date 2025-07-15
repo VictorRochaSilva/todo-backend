cf934243cbdd18c68294ea73bbac065e1c486f47
Victor Rocha <victowrs.rocha@gmail.com>
2025-07-15 18:26:27 -0400

    atualizando

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { app } from './config/app';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8085;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
