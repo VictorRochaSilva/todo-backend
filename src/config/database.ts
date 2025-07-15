874476e3ab6864a662447165ac014009c6eeb33f
Victor Rocha <victowrs.rocha@gmail.com>
2025-07-15 19:58:57 -0400

    voltando codigo que seleciona o banco

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-const mongoUri = "mongodb://admin:senha123@mongodb:27017/admin?authSource=admin";
+const mongoUri = "mongodb://admin:senha123@mongodb:27017/?authSource=admin";
 
 async function connectMongo() {
     if (mongoose.connection.readyState >= 1) return;
 
     try {
-        await mongoose.connect(mongoUri);
+        await mongoose.connect(mongoUri, { dbName: "admin" });
         console.log("✅ Conectado ao MongoDB!");
     } catch (err) {
         console.error("❌ Erro ao conectar no MongoDB:", err);
